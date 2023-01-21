61482e4368b6996428cd2a48f6ad52c2051a0e4b
linduix <abdullahhkhan3@gmail.com>
2023-01-22 01:57:11 +0500

    small adjustments

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,24 @@
+'''
+Module Name: main.py
+
+This is the main script for the flask backend server
+'''
+
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource
 
 
 class Hello(Resource):
+    '''Api Hello endpoint'''
+
     def get(self):
+        '''get method'''
         name = request.args.get('name')
         print(name)
         return {"hello": name if name else "world"}
 
     def post(self):
+        '''post method'''
         pass
 
 
@@ -18,7 +28,8 @@ api.add_resource(Hello, '/hello')
 
 
 @app.route('/hi', methods=['GET'])
-def hi():
+def hi_endpoint():
+    '''server endpoint methood'''
     return "hello"
 
 
