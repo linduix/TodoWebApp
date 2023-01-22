01069de5aa398b0afc55f8ccd4cc42bbd03d278a
linduix <abdullahhkhan3@gmail.com>
2023-01-22 19:32:31 +0500

    switched from pip to poetry

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,37 @@
-'''
+"""
 Module Name: main.py
 
 This is the main script for the flask backend server
-'''
+"""
 
 from flask import Flask, request
 from flask_restful import Api, Resource
 
 
 class Hello(Resource):
-    '''Api Hello endpoint'''
+    """Api Hello endpoint"""
 
     def get(self):
-        '''get method'''
-        name = request.args.get('name')
+        """get method"""
+        name = request.args.get("name")
         print(name)
         return {"hello": name if name else "world"}
 
     def post(self):
+        """post methood"""
         body = request.json
 
 
 app = Flask(__name__)
 api = Api(app)
-api.add_resource(Hello, '/hello')
+api.add_resource(Hello, "/hello")
 
 
-@app.route('/hi', methods=['GET'])
+@app.route("/hi", methods=["GET"])
 def hi_endpoint():
-    '''server endpoint methood'''
+    """server endpoint methood"""
     return "hello"
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run()
